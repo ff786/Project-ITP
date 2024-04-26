5c9d07f242dd9f7265203928985b4aae9f12c9e7
M_Farsith <mohammed.farsith@gmail.com>
2024-04-26 13:46:23 +0530

    Commit Today Time : 01:46 PM Fixed view of NotifyView feat/Notifications

--- src/Components/NotifyView/NotifyView.jsx
+++ src/Components/NotifyView/NotifyView.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Topbar from '../common/topbar/Topbar.jsx'
-import Sidebar from '../common/sidebar/Sidebar.jsx'
+import SideNav from '../common/SideNav/sideNav.jsx'
 import axios from 'axios';
 import '../NotifyForm/NotifyForm.css'
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import { useNavigate } from "react-router-dom";
 
 function NotifyView() {
@@ -29,27 +30,31 @@ function NotifyView() {
     return (
         <div>
             <Topbar />
-            <div className="md">
+            <div>
                 <div>
-                    <Sidebar />
+                    <SideNav />
                 </div>
-                <div className="flex justify-items-center" style={{ width: '100%' }}>
+                <div style={{marginTop: '90px', display: 'flex', position: 'relative'}}>
+                <div className="flex justify-items-center" style={{ width: '100%', position: 'relative', padding: '10px'}}  >
                     <div className="max-w-4xl mx-auto p-4">
-                        <div className="flex justify-between items-center mb-4">
-                            <h1 className="text-xl font-bold text-zinc-800 dark:text-black">Sent Notifications</h1>
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={() => navigate('/NotifyForm')}>Create New</button>
+                        <div className="flex justify-between items-center mb-4" style={{ maxHeight: '80vh', overflowY: 'auto' }}>
+                            <h1 className="text-xl font-bold text-black-900 dark:text-black">Sent Notifications</h1>
+                            <button type="submit"
+                            className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={() => navigate('/NotifyForm')}
+                            style={{ fontFamily: 'arial', fontSize: '18px' }}
+                            >Create New</button>
                         </div>
 
                         {data.map(item => (
-                            <div key={item.id} className="bg-white dark:bg-zinc-700 shadow rounded-lg p-4 mb-4">
+                            <div key={item.id} className="bg-white dark:bg-white-700 shadow-xl rounded-lg p-4 mb-4">
                                 <div className="flex justify-between items-center mb-3">
-                                    <h1 className="text-lg font-semibold text-zinc-800 dark:text-white">{item.subject}</h1>
-                                    <span className="text-sm text-zinc-500 dark:text-zinc-300">{item.deliveredTime.substring(0, item.deliveredTime.indexOf('T'))} | {item.deliveredTime.substring(item.deliveredTime.indexOf('T') + 1, item.deliveredTime.length)}</span>
+                                    <h1 className="text-lg font-semibold text-zinc-800 dark:text-black">{item.subject}</h1>
+                                    <span className="text-sm text-black-600 dark:text-black-300">{item.deliveredTime.substring(0, item.deliveredTime.indexOf('T'))} | {item.deliveredTime.substring(item.deliveredTime.indexOf('T') + 1, item.deliveredTime.length)}</span>
                                 </div>
                                 <div>
-                                    <h1 className="text-sm font-semibold text-zinc-800 dark:text-white">TO : {item.firstName} {item.lastName}</h1>
+                                    <h1 className="text-sm font-semibold text-zinc-800 dark:text-black">TO : {item.firstName} {item.lastName}</h1>
                                 </div>
-                                <p className="text-zinc-600 dark:text-zinc-200 mb-3">
+                                <p className="text-zinc-600 dark:text-black-200 mb-3">
                                     {item.message.substring(item.message.lastIndexOf('*') + 1, item.message.length).trim()}
                                 </p>
                                 <div className="flex items-center justify-between">
@@ -66,20 +71,11 @@ function NotifyView() {
                                     </div>
                                     <div className="flex space-x-2">
                                         <button onClick={() => {navigate('/NotifyForm', {state: {data: item}})}}
-                                                className="p-2 text-zinc-600 hover:text-zinc-700 dark:text-zinc-300 dark:hover:text-white">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25V9M6 10.5v7.875a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25V10.5m-12 0h12m-9 0V5.25A2.25 2.25 0 0110.5 3h3a2.25 2.25 0 012.25 2.25V10.5m-9 0h12"/>
-                                            </svg>
+                                                className="p-2 text-black hover:text-green-700 dark:text-green-300 dark:hover:text-green">
+                                            <FaEdit />
                                         </button>
-                                        <button
-                                            className="p-2 text-zinc-600 hover:text-zinc-700 dark:text-zinc-300 dark:hover:text-white">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M6 18.75L18 6.75m0 12L6 6.75m1.5 0h9m-9 12h9"/>
-                                            </svg>
+                                        <button className="p-2 text-black hover:text-red-700 dark:text-red-300 dark:hover:text-red">
+                                            <FaTrash />
                                         </button>
                                     </div>
                                 </div>
@@ -87,6 +83,7 @@ function NotifyView() {
                         ))}
 
                     </div>
+                </div>
                 </div>
             </div>
         </div>
