b6fccc8b236124a7f8b19e5a2275d3000e77f43d
sahangamage15 <128965479+sahangamage15@users.noreply.github.com>
2024-05-02 23:25:40 +0530

    dev-sahan

--- src/Components/ViewPND/PndView.jsx
+++ src/Components/ViewPND/PndView.jsx
@@ -3,11 +3,11 @@ import logo from '../common/header/logo.png'
 import icon_prof from '../ClaimManage/icon_prof.png'
 import notify from '../ClaimManage/notify.png'
 import '../../App.css'
-import '../ClaimManage/ClaimForm.css'
+import './PndView.css'
 import Sidebar from '../common/sidebar/Sidebar.jsx'
 import search from '../common/search/Search.jsx'
 import '../PnD/Diagnosis.css'
-import '../ClaimOverview/overView.css'
+import './PndOverview.css'
 import Topbar from '../common/topbar/Topbar.jsx'
 
 
@@ -63,17 +63,17 @@ function CodeModify({ searchQuery }) {
                 </div>
 
                 <div className="mid">
-                    <div class="container mx-auto px-4">
-                        <div class="flex justify-between items-center py-4">
-                            <h1 class="text-2xl font-semibold">Procedure and Diagnosis codes</h1>
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <div className="container mx-auto px-4">
+                        <div className="flex justify-between items-center py-4">
+                            <h1 className="text-2xl font-semibold">Procedure and Diagnosis codes</h1>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 Add Code
                             </button>
                         </div>
-                    <div class="flex justify-between mb-4">
-                        <div class="flex gap-2 items-center">
+                    <div className="flex justify-between mb-4">
+                        <div className="flex gap-2 items-center">
                             <label>Show</label>
-                            <select class="form-select block w-full mt-1 border-zinc-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
+                            <select className="form-select block w-full mt-1 border-zinc-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
                                 <option>10</option>
                                 <option>20</option>
                                 <option>50</option>
@@ -81,19 +81,19 @@ function CodeModify({ searchQuery }) {
                             <label>entries</label>
                         </div>
                         <div>
-                            <input type="text" placeholder="Search:" class="form-input block w-full mt-1 border-zinc-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md" />
+                            <input type="text" placeholder="Search:" className="form-input block w-full mt-1 border-zinc-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md" />
                         </div>
                     </div>
 
-                    <div class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
-                        <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+                    <div className="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
+                        <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
                             <thead>
-                                <tr class="text-left">
-                                    <th class="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Code</th>
-                                    <th class="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Code Type</th>
-                                    <th class="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Title</th>
-                                    <th class="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Description</th>
-                                    <th class="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Action</th>
+                                <tr className="text-left">
+                                    <th className="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Code</th>
+                                    <th className="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Code Type</th>
+                                    <th className="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Title</th>
+                                    <th className="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Description</th>
+                                    <th className="py-2 px-3 sticky top-0 border-b border-zinc-200 bg-zinc-100">Action</th>
                                 </tr>
                             </thead>
                             <tbody className="bg-white dark:text-white">
@@ -130,13 +130,13 @@ function CodeModify({ searchQuery }) {
                         </table>
                     </div>
 
-                    <div class="flex justify-between items-center py-4">
+                    <div className="flex justify-between items-center py-4">
                         <span>Showing 1 to 2 of 2 entries</span>
-                        <div class="inline-flex">
-                            <button class="text-sm bg-zinc-300 hover:bg-zinc-400 text-zinc-800 font-semibold py-2 px-4 rounded-l">
+                        <div className="inline-flex">
+                            <button className="text-sm bg-zinc-300 hover:bg-zinc-400 text-zinc-800 font-semibold py-2 px-4 rounded-l">
                                 Previous
                             </button>
-                            <button class="text-sm bg-zinc-300 hover:bg-zinc-400 text-zinc-800 font-semibold py-2 px-4 rounded-r">
+                            <button className="text-sm bg-zinc-300 hover:bg-zinc-400 text-zinc-800 font-semibold py-2 px-4 rounded-r">
                                 Next
                             </button>
                         </div>
