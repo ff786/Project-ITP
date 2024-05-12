2e52b0b5627cd787189d9fc0c21d17ad3bd74241
ArunaluB <it22515612@my.sliit.lk>
2024-05-12 13:51:23 +0530

    iiiiiiii

--- src/Components/Inventory/inventoryexpiremedicinetab.jsx
+++ src/Components/Inventory/inventoryexpiremedicinetab.jsx
@@ -43,6 +43,7 @@ const inventoryexpiremedicinetab = () => {
         <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-500 hover:bg-red-100 hover:text-red-800 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-red-800/30 dark:hover:text-red-400" onClick={() => handleDelete(row.id)}>
           Delete
         </button>
+        
 
       )
     }
